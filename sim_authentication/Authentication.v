89fbcda0e6f4441db850821005a6ca4d8b908042
Avish2992 <80486350+Injuredhunter@users.noreply.github.com>
2021-04-21 16:21:28 -0500

    Add files via upload
    Final version of Authentication Top Level Module Code, fully tested and WORKING on Model Sim Testbench.

--- sim_authentication/Authentication.v
+++ sim_authentication/Authentication.v
@@ -6,36 +6,27 @@
 // clk: internal 50 MHz clock on FPGA Hardware
 // rst: reset physical push-button on FPGA Hardware (inverse logic), 
 // 	reset all values back to default
-// pwdigit: 4-bit user-input from toggle switches to type in the password
+// pwdigit: 4-bit user-input from toggle switches to type in the User ID/Password
 // pwenter: a push-button coming from the button-shaper as a single cycle pulse
-// 	    to enter the typed in password value
-//          afterwords logged in: used to Start the game. 
-// load_p1_in: a push-button coming from the button-shaper as a single cycle pulse
-//	       enters the types in value from player 1's set of switches.
-// load_RNG_in: a push-button, without using a button-shaper,
-// 		enters a random value based on how long user pushed the button
-// timeout: A flag notifying this module when the timer for the game has run out.
+// 	    to enter the typed in ID/PW value
+// log_out_ctrl: input from GameController sending pulse to PW Module when to log out.
 
 // OUTPUTS
-// logged_in/logged_out: Status if the user has been logged into the system.
-// load_p1_out/load_RNG_out: allows the values to be passed through to other modules when 1.
-// timer_reconfig: tells the timer to reset values back to default values
-// 		   without having to reset the whole board
-// timer_enable: tells the timer when to start counting.
+// log_in_ctrl: output sent to GameController showing when User is logged in.
+// isGuest_ctrl: passthrough output sent to GameController showing if it is a guest ID/PW
+// intID_ctrl: 3-bit integer passthrough output sent to GameController showing which User is playing.
 //
-// REG
-// State: The current state of the finite state machine.
-// Sofarsogood: a flag that keeps checking to see if the user has typed in any
-//		incorrect password values.
+// WIRE
+// log_out: Passthrough from log_in_ctrl, GameController, PW and ID Modules all log out.
+// matchID: if ID is matching, high flag sent from ID to PW Module.
+// isGuest: flag sent from ID Module to PW Module, indicating if using guest ID/PW.
+// intID: 3-bit integer sent from ID Module to PW Module showing which User is playing.
+// addr_UID_ROM/addr_PW_ROM: 5-bit address sent to UID/PW ROM to fetch for ID/PW
+// q_UID_ROM/q_PW_ROM: 16-bit/24-bit Integer grabbed from UID/PW ROM.
 //
 // HOW TO UNLOCK
-// The user needs to type in password values to log into the system so they can play the game.
-// After the system has been unlocked, the user will be logged in:
-// the logged_in flag will be on and logged_out flag will be off
-// player 1 and RNG inputs will transfer through to other modules allowing to play the game.
-//
-// Note: Currently, the password is set to 9989 in order to test my system.
-// Status: SUCCESSFUL for both Wrong Passwords and Correct Passwords.
+// The user needs to type in 4-digit username followed by a 6-digit password to log into the system.
+// a push-button must be pressed after each digit has been configured via toggle switches.
  
 module Authentication(clk, rst, pwdigit, pwenter, log_out_ctrl, log_in_ctrl, isGuest_ctrl, intID_ctrl);
 
@@ -49,17 +40,18 @@ module Authentication(clk, rst, pwdigit, pwenter, log_out_ctrl, log_in_ctrl, isG
    wire [2:0] intID;
 
    wire [4:0] addr_UID_ROM, addr_PW_ROM;
-   wire [15:0] q_UID_ROM, q_PW_ROM;   
+   wire [15:0] q_UID_ROM;
+   wire [23:0] q_PW_ROM;   
 
-   ROM_UID  User_ROM(addr_UID_ROM, clk, q_UID_ROM);
-   ROM_PWD  Password_ROM(addr_PW_ROM, clk, q_PW_ROM);
+   UID_ROM  User_ROM(addr_UID_ROM, clk, q_UID_ROM);
+   PW_ROM  Password_ROM(addr_PW_ROM, clk, q_PW_ROM);
 
    ID_Checking   UserEnter(clk, rst, pwdigit, pwenter, log_out, 
                            matchID, isGuest, intID,
-                           addr_ROM, q_ROM);
+                           q_UID_ROM, addr_UID_ROM);
 
    PW_Checking   PasswordEnter(clk, rst, pwdigit, pwenter, matchID, isGuest, intID, log_out_ctrl,
                                log_out, isGuest_ctrl, intID_ctrl, log_in_ctrl, 
-                               addr_ROM, q_ROM);
+                               q_PW_ROM, addr_PW_ROM);
 
 endmodule
